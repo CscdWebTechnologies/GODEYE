7c52bdf97a281284f34260022efd9ea716b1e8e2
Dawuda Nuraini <lawsondawuda6@gmail.com>
2021-05-16 05:00:34 +0000

    First stage in coinbase clone. Toolbar built exactly as in coinbase with its exact dimensions and colourations. The next stage will be to implement a submenu that appears and disappears with mouseover and mouseout over the menus

---
+++ App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import React from "react";
+
+
+class App extends React.Component {
+  
+  
+
+  
+  render() {
+
+  return <div>
+    
+    <div className="menu">
+            <div className="title">
+        coinbase </div>
+              <div className="nav">
+              <a href=""><div className="list">Prices</div></a>
+              <a href=""><div className="list1" >Learn</div></a>
+                  <a href=""><div className="list2">Individual</div></a>
+                  <a href=""><div className="list3">Businesses</div></a>
+                  <a href=""><div className="list4">Developers</div></a>
+                  <a href=""><div className="list5">Company</div></a>
+  
+              </div>
+
+                <div className="signAndSearch">
+                  <div className="signIn">Sign in
+                  </div>
+                      <button className="search">Get Started
+                      </button>
+                </div>
+
+                </div>
+
+
+</div>
+
+
+
+  }
+
+
+}
+
+
+
+
+export default App;
