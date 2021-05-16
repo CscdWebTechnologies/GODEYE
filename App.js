5637e88d5967202587e5b9bebb2a3e82d8940f2a
Dawuda Nuraini <lawsondawuda6@gmail.com>
2021-05-16 10:52:05 +0000

    Implemented the cion table in coinbase

--- App.js
+++ App.js
@@ -414,7 +414,59 @@ else
             <div className="circle"></div>
             <div className="mobile"><img className="mobileImg" src="https://play-lh.googleusercontent.com/2k2zVCdzp88js2i-BlNKNJpuHTqO0k2G67BmW_8R9caBO33evmywx093W1PGnY10zKF2"/></div>
 
+
+
+
+
+            <table>
+              <tr className="head">
+                <th>#</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Change</th>
+                <th>Chart</th>
+                <th>Trade</th>
+              </tr>
+              <tr>
+                <td className="Numbers">1</td>
+                <td><img className="bit" src="https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png"/><span className="coin">Bitcoin </span><span className="b">BTC</span></td>
+                <td className="price">GHS 324,401.28</td>
+                <td className="change">+1.27%</td>
+                <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3iY4ne7lULSq1xpbYlufYGqDZPY134kZUDQ&usqp=CAU"/></td>
+                <td ><button className="buy">Buy</button></td>
+              </tr>              
+              <tr>
+                <td className="Numbers">2</td>
+                <td><img className="bit"  src="https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png"/> <span className="coin">Ethereum</span><span className="b">ETH</span></td>
+                <td className="price">GHS 24,637.82</td>
+                <td className="change">+8.07%</td>
+                <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3iY4ne7lULSq1xpbYlufYGqDZPY134kZUDQ&usqp=CAU"/></td>
+                <td ><button className="buy">Buy</button></td>
+              </tr>                      <tr>
+                <td className="Numbers">3</td>
+                <td><img className="bit"  src="https://dynamic-assets.coinbase.com/93a4303d1b0410b79bb1feac01020e4e7bdf8e6ece68282d0af2c7d0b481c5f5c44c0cec1d7071ae8f84674dbd139e290d50a038a6a4c1bbc856ec0871b5f3e2/asset_icons/3af4b33bde3012fd29dd1366b0ad737660f24acc91750ee30a034a0679256d0b.png"/> <span className="coin">Bitcoin cash</span><span className="b">BTH</span></td>
+                <td className="price">GHS 8,471.21</td>
+                <td className="change">+6.79%</td>
+                <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3iY4ne7lULSq1xpbYlufYGqDZPY134kZUDQ&usqp=CAU"/></td>
+                <td ><button className="buy">Buy</button></td>
+              </tr>                      <tr>
+                <td className="Numbers">4</td>
+                <td><img className="bit"  src="https://dynamic-assets.coinbase.com/f018870b721574ef7f269b9fd91b36042dc05ebed4ae9dcdc340a1bae5b359e8760a8c224bc99466db704d10a3e23cf1f4cd1ff6f647340c4c9c899a9e6595cd/asset_icons/984a4fe2ba5b2c325c06e4c2f3ba3f1c1fef1f157edb3b8ebbfe234340a157a5.png"/><span className="coin">Litecoin </span><span className="b">LTC</span></td>
+                <td className="price">GHS 2,121.98</td>
+                <td className="change">+3.50%/</td>
+                <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3iY4ne7lULSq1xpbYlufYGqDZPY134kZUDQ&usqp=CAU"/></td>
+                <td ><button className="buy">Buy</button></td>
+              </tr>        
+            </table>
+
+
+
+
+
                   </div>
+
+
+
 </div>
 
 
